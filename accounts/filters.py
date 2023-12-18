0cfb29622da5c6a70311cb754270b9965ae0fe4f
krsnamara <martinj.fitzpatrick@gmail.com>
2023-12-18 11:25:01 -0500

    Add widgets to filters.py and style. Change Labels for inputs

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,12 +1,14 @@
 import django_filters
+from django import forms
 from django_filters import DateFilter, CharFilter
 
 from .models import *
 
 class OrderFilter(django_filters.FilterSet):
-    start_date = DateFilter(field_name="date_created", lookup_expr="gte")
-    end_date = DateFilter(field_name="date_created", lookup_expr="lte")
-    note = CharFilter(field_name="note", lookup_expr="icontains")
+    start_date = DateFilter(field_name="date_created", lookup_expr="gte" , label="Start Date", widget=forms.DateInput(attrs={ 'style': 'max-width: 150px;'}), )
+    end_date = DateFilter(field_name="date_created", lookup_expr="lte", label="End Date", widget=forms.DateInput(attrs={ 'style': 'max-width: 150px;'}),)
+    note = CharFilter(field_name="note", lookup_expr="icontains", widget=forms.TextInput(attrs={ 'style': 'max-width: 100px;'}),)
+
     
     class Meta:
         model = Order
